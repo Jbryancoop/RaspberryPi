62ae9328349ae0364c949c1a006cf67c41727342
Jeff Cooper <Jeff@JeffsMBP.local>
2016-04-23 13:20:11 -0600

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+x = input("Enter a number:\n\n ")
+print('\n')
+y = input("Enter a number:\n\n ")
+print('\n')
+z = input("Enter a number:\n\n ")
+print('\n')
+
+lst = [x,y,z]
+lst.sort()
+print (lst)
+
+
